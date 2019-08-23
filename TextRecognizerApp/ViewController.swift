ac8c2f44cddec21978949e731aa4a8d6a6ff2f9e
mtuzer <tuzermert@gmail.com>
2019-08-23 21:06:36 +0300

    Initial Commit

---
+++ TextRecognizerApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TextRecognizerApp
+//
+//  Created by Mert Tuzer on 23.08.2019.
+//  Copyright © 2019 Mert Tuzer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
